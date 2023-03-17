2b99212d38b9456d47a11d9b0528edbdf160d2b6
vishal thayyil <vishalthayyil@gmail.com>
2023-03-17 15:02:10 +0530

    .....

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ const slideUp = () => {
   const slideUpElements = document.querySelectorAll('.slide-up');
 
   const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
+    for (let i = 0; i < entries.length; i++) {
+      const entry = entries[i];
       if (entry.isIntersecting) {
         const target = entry.target;
-        const delay = Array.from(slideUpElements).indexOf(target) * 80;
+        const delay = i * 80;
         target.style.transitionDelay = `${delay}ms`;
         target.classList.add('show');
         observer.unobserve(target);
       }
-    });
+    }
   });
 
   slideUpElements.forEach((element) => {
@@ -19,6 +20,10 @@ const slideUp = () => {
 };
 
 slideUp();
+
+
+
+
 
   
 
